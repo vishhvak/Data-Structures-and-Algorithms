3f40aef79c5ed121db3308ba4502abb5ebac4add
Vishhvak <cswithvish@gmail.com>
2018-08-30 23:41:50 +0530

    Synchronization

--- OpenMP/prod_cons.c
+++ OpenMP/prod_cons.c
@@ -3,25 +3,25 @@
 
 int main()
 {
-  int a[50], i;
-  for(i=0;i<50;i++) {
-    a[i] = i;
+  int a[15], i;
+  for(i=0;i<15;i++) {
+    a[i] = 2*i;
   }
   #pragma omp parallel sections
   {
     #pragma omp section
     {
       #pragma omp critical
-      for(i=0;i<20;i++)
+      for(i=0;i<15;i++)
       {
-        a[i]=50-i;
+        a[i]=40-i;
         printf("Produced %d\n",a[i]);
       }
     }
     #pragma omp section
     {
       #pragma omp critical
-      for(i=0;i<20;i++)
+      for(i=0;i<15;i++)
       {
         if(a[i]==0)
           printf("Empty Queue\n");
