21a92c37aec0140da251bf617ed0503916349541
Vishhvak <cswithvish@gmail.com>
2018-09-18 21:41:16 +0530

    More OpenMP

--- OpenMP/openmp17.c
+++ OpenMP/openmp17.c
@@ -1,14 +1,13 @@
 
 #include <stdio.h>
 #include <omp.h>
-#pragma omp_set_nested(true)
 
 int main()
 {
   int i, j, k;
   int m1[100][100], m2[100][100], mul[100][100];
   //omp_set_num_threads(8);
- 
+  omp_set_nested(1);
   for (i = 0; i < 100; i++)
     for (j = 0; j < 100; j++)
       m1[i][j]=i+j;
@@ -21,12 +20,13 @@ int main()
     for (j = 0; j < 100; j++)
       mul[i][j]=0;
 
-  #pragma omp parallel for collapse(3)
+  #pragma omp parallel for 
   for (i = 0; i < 100; i++) {
+    #pragma omp parallel for //2nd case
     for (j = 0; j < 100; j++) {
+      //#pragma omp parallel for //(3rd case)
       for (k = 0; k < 100; k++) {
         mul[i][j] += m1[i][k]*m2[k][j];
-        printf("\nInside thread %d\n",omp_get_thread_num());
       }
     }
   }
